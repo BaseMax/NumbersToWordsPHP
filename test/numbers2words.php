d75f69c8b3c24e97a39ec0423696ae7a01dba7ee
Max Base <MaxBaseCode@Gmail.Com>
2019-03-15 10:08:45 +0330

    Update numbers2words.php

--- test/numbers2words.php
+++ test/numbers2words.php
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : NumbersToWords.php
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-03-15
+* @Released under : https://github.com/BaseMax/NumbersToWordsPHP/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/NumbersToWordsPHP
+*
+**/
